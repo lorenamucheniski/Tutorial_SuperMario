b0f1d7dd40cb79bf961e69bc8be77cffdaf346de
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-25 16:21:44 -0300

    feat: 25-01 adicionando conteúdo a página/ mudando alguns estilos, responsivo/ adicionando conteúdo a página 'conheça o scratch'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dataEHora = document.querySelector('.data_hora');
-const data = new Date('2024-01-24 15:30');
+const data = new Date('2024-01-25 16:15');
 
 function getDiaSemanaTexto(diaSemana) {
 
