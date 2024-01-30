dbb6c896dcb4b9828c2aaaed3884051e468e610d
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-30 15:50:35 -0300

    feat: 30-01 alterações de conteúdo e css das páginas

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ const imagem = document.querySelector('.img_tela_cheia');
 imagem.addEventListener('click', function(){
     this.classList.toggle('tela_cheia');
 });
-
-function adicionarClasseNosElementos (){
-    let elementos = document.querySelector('*');
-}
